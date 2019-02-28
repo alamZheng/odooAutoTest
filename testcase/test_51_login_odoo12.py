673b01a16d47d5a2ac60616d33db262fd0fb1d90
676239802@qq.com <alamZheng>
2019-02-28 15:55:19 +0800

    util screenshow

--- testcase/test_51_login_odoo12.py
+++ testcase/test_51_login_odoo12.py
@@ -1,31 +1,45 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+import time
+from utils.Logger import Log
+from utils.Screenshot import Screen
+from PO.login_page_odoo12 import LoginPage
 import unittest
 from selenium import webdriver
 import sys
 sys.path.append("..")
-from PO.login_page_odoo12 import LoginPage
-from utils.Util_screenshot import Screen
-import time
+
+log = Log()
+
 
 class TestLogin(unittest.TestCase):
-    imgs=[]
+
+    imgs = []
     driver = webdriver.Chrome()
     """UI自动化登录"""
-    def setUp(self):
+
+    @classmethod
+    def setUpClass(self):
+        pass
         # print("setup")
         # url = "http://192.168.100.26:8069/web/login"
-        self.driver = webdriver.Chrome()
+        # self.driver = webdriver.Chrome()
         # self.driver = webdriver.PhantomJS()
         # self.driver.implicitly_wait(5)
         # self.verificationErrors = []
 
     def tearDown(self):
+        sp = LoginPage(self.driver)
+        sp.logout()
+
+    @classmethod
+    def tearDownClass(self):
         self.driver.quit()
 
     @Screen(driver)
     def test_login_01_default(self):
         """登录"""
+        log.info("---test_login_01_default----")
         sp = LoginPage(self.driver)
         # sp.open(self.url)
         sp.login()
@@ -33,14 +47,14 @@ class TestLogin(unittest.TestCase):
         sp.add_img(self.imgs)
         # self.assertEqual(sp.get_username(),"hanxiaobei",msg="验证失败！")
 
-    @unittest.skip("had tested")
+    # @unittest.skip("had tested")
     def test_login_02_customer(self):
         """登录自定义的账户和密码"""
-        # print("111")
+        log.info("---test_login_02_customer----")
         sp = LoginPage(self.driver)
 
         # sp.open(self.url)
-        sp.login("admin","admin")
+        sp.login("admin", "123456")
         # self.add_img()
         sp.add_img(self.imgs)
         # self.assertEqual(sp.get_username(),"hanxiaobei",msg="验证失败！")
@@ -52,11 +66,11 @@ class TestLogin(unittest.TestCase):
         sp = LoginPage(self.driver)
 
         # sp.open(self.url)
-        sp.login("andy.yang@aqara.com","111111")
+        sp.login("andy.yang@aqara.com", "111111")
         # self.add_img()
         sp.add_img(self.imgs)
         print(sp.get_alert_msg())
-        self.assertEqual(sp.get_alert_msg(),"错误的登录名/密码",msg="验证失败！")
+        self.assertEqual(sp.get_alert_msg(), "错误的登录名/密码", msg="验证失败！")
 
     @unittest.skip("had tested")
     def test_login_04_wrongUser(self):
@@ -65,11 +79,16 @@ class TestLogin(unittest.TestCase):
         sp = LoginPage(self.driver)
 
         # sp.open(self.url)
-        sp.login("test.yang@aqara.com","123456")
+        sp.login("test.yang@aqara.com", "123456")
         # self.add_img()
         sp.add_img(self.imgs)
         # print(sp.get_alert_msg())
-        self.assertEqual(sp.get_alert_msg(),"错误的登录名/密码",msg="验证失败！")
+        self.assertEqual(sp.get_alert_msg(), "错误的登录名/密码", msg="验证失败！")
+
+    # def test_05_logout(self):
+    #     sp = LoginPage(self.driver)
+    #     sp.login()
+    #     sp.logout()
 
 
 if __name__ == '__main__':
